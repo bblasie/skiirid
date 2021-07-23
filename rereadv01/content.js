d30d916f942da0d1fd7e943235f97f569a87a429
Blaise M <blaise.jo@gmail.com>
2021-07-22 18:13:32 -0700

    minor improvements
    Stripping existing code and adding notes.

--- rereadv01/content.js
+++ rereadv01/content.js
@@ -1,12 +1,22 @@
+// Inception of expanse in knowledge.
+// Everyone is allowed to contribute whereever they see fit.
+// Have fun and stay 
 
+
+
+// for extracting the contents of a site.
+// Start_ premade for just one site, try to find use or remove or expand per requested site. (latter sounds nice.)
 let sidePanel = document.getElementsByClassName("mw-panel");
 sidePanel["visibility"] = "collapse";
 // let sidePanel1 = document.getElementsByClassName("mw-panel");
 // sidePanel1["visibility"] = "hidden";
 let side = document.getElementsByTagName("nav");
 side["visibility"] = "none";
+// End_ need this expanded into a class or some useful extractor. keep open for api. :wink:
+
 
 ////////////////////////////////////////////////////
+// Start__ global Vars.find way to closure most of them. func-em. :smile: 
 let typed = "";let highlightTextCount;let circleY;let paragraphs = [];let aLinks = [];let aLinksText = [];let wPicked = [];let keeper = [];let coords = [];let allTypedStorage;let secondCopy = 0;let randLink;let btnShow;let fiveC;let r;let a, movedown, sidespeed, xinit, xcoverinit;let xfin, yinit, yfin, step;let now;let sizeOfCounter;let sizeOfText;let tt = "";let wContainer = "";let words;let k;let howMuch;let p;let leadBG;let displayT;let limit;let xPlace;let yPlace;
 // let yPlaceTop;
 let yPlaceBottom;let lightPlacebottom;let posWheel;
@@ -14,13 +24,14 @@ let yPlaceBottom;let lightPlacebottom;let posWheel;
 // buttons.
 let click1;let speedDown, speedUp;let click2;let click3;let curWordW;let word2Vec;let aiSuggestOne;let aiSuggestTwo;let aiSuggestThree;let aiSuggestFour;let aiSuggestFive;let allLinks;let enter;
 let tFont = 'Georgia';
-// When the model is loaded
-function modelLoaded() {
-  window.alert('Model Loaded!');
-}
+// End__ global Vars.find way to closure most of them. func-em. :smile:
+
+
+// Needs work._____________________-
+// find ways to shrink this func.
+// Start___
 function setup() {
   background(120, 160, 120);
-
   // below is where we collect all the paragraphs
   paragraphs = document.getElementsByTagName('p');
   for (elt of paragraphs) {
@@ -31,9 +42,7 @@ function setup() {
       wContainer += paragraphs[i].innerText;
       // wContainer = join(paragraphs[i].innerText, wContainer)
       tt += paragraphs[i].innerText;
-      
     }
-    
   } else {
     noCanvas(); let displayT = [];
     // console.log("There is not ext on this page.")
@@ -45,54 +54,28 @@ function setup() {
   for (elt of aLinks) {
       append(aLinksText, elt.text)
   }
-
-
   ///////////////////////////////////////
   // Instantiate the needed variables.
-
-  p = 18;
-  leadBG = 190;
-  displayT = [];
-  allTypedStorage = [];
-  limit = 2012;
-  xPlace = windowWidth * 0.1;
-  yPlace = windowHeight * 0.13;
-  yPlaceBottom = windowHeight * 0.78;
-  lightPlacebottom = windowHeight * 0.78;
-  movedown = windowHeight / 2 - 80;
-  yinit = windowHeight * 0.2 - 30;
-  yfin = windowHeight * 0.8;
-  xfin = windowWidth * 4 / 6;
-  sizeOfCounter = 0;
-  xcoverinit = -500;
-  sidespeed = 10;
-  now = false;
-  xinit = 5;
-  step = windowWidth * 0.1;
-  a = 30;
-  fiveC = (xfin+120)*1/5;
-  randLink = random(aLinksText.length);
-  keeper = [];
-  highlightTextCount = 1;
-  posWheel = 30;
-  btnShow = true;
-  enter = false;
-  divider(tt);
-  noLoop();
+  p = 18;leadBG = 190;displayT = [];allTypedStorage = [];limit = 2012;xPlace = windowWidth * 0.1;yPlace = windowHeight * 0.13;yPlaceBottom = windowHeight * 0.78;
+  lightPlacebottom = windowHeight * 0.78;movedown = windowHeight / 2 - 80;yinit = windowHeight * 0.2 - 30;yfin = windowHeight * 0.8;xfin = windowWidth * 4 / 6;sizeOfCounter = 0;xcoverinit = -500;sidespeed = 10;now = false;xinit = 5;step = windowWidth * 0.1;a = 30;fiveC = (xfin+120)*1/5;randLink = random(aLinksText.length);keeper = [];highlightTextCount = 1;posWheel = 30;openLink = null;enter = false;divider(tt);noLoop();
+  r = round(random(aLinks.length));
   return;
 }
+// End___ 
+
+
+// Needs Work. * to be more efficient with more functionalities.
 function mouseWheel(event) {
   print(event.delta);
   //move the square according to the vertical scroll amount
   posWheel += event.delta;
-  }
-
-// divide the text into smaller chunks.
+}
+// needs work. * turn into a class. * lowercase first and move 
+// divide the text into smaller chunks.  
 function divider(wContainer) {
   console.log("reached divider.");
   words = wContainer.split(" ");
   let x = 0;
-
   let t = [];
   displayT = [];
   // words = displayT.split(displayT, ".");
@@ -110,13 +93,11 @@ function divider(wContainer) {
   })
   displayT.push(t.join(" "));
   k = displayT.length;
-
   x = 0;
   t = [];
   console.log('gave displayT the following before leaving divider \n\n', displayT , " with len: ", displayT[0].length);
   return
 }
-
 // display lines to the canvas.
 function lines(htc, typed) {
   textFont('Georgia');
@@ -130,7 +111,7 @@ function lines(htc, typed) {
   typed = typed;
 
   let wordW = 0;
-  if (true) {
+  //if (true) {
     // show me the box to cover the element.
     fill(173, 216, 230);
     textAlign(LEFT);
@@ -149,7 +130,7 @@ function lines(htc, typed) {
     for (let i = 0; i < specialW.length; i++) {
       // get the width of each word
       curWordW = textWidth(specialW[i]);
-      
+
       //let curWordW = textWidth(specialW[i]);
       console.log("running in line changing color: " + typed);
       //displayT = paragraph.split(" ");
@@ -173,65 +154,40 @@ function lines(htc, typed) {
         wordWY += 30;
       }
     }
-
     linkPosX= round(windowWidth*(1/7), 2);
     linkPosY = round(windowHeight*0.8 + 70, 2);
-    if (!btnShow)return;
     let aLinks = document.getElementsByTagName('a');
     // The for loop below picks all the links that on the page.
     for (elt of aLinks) {
-        //elt.style['background-color'] = 'orange';
-        // aLinksText.push(elt.text);
         append(aLinksText, elt.text)
     }
-
-    for (let i=1; i < 7; i++){
-      r = random(aLinksText.length);
-      r = round(r);
-      console.log("opened: " + aLinks[r]);
-      console.log("--random num generated for: " + i + " --is: "+ r +" --with link name: " + aLinksText[r] + " --and link address: " + aLinksText[r] + "\n\n"); 
-      for (let i=0;i<aLinks.length;i++){
-        if (aLinksText[r].length > 4 && aLinksText[r].text === r && aLinksText[r].innerHTML){
-            console.log("found chelsea in the page and their link is: " + aLinks[r].href)
-            window.open(aLinks[r].href);
-        }
-      }
-      fill(255);
+      //r = round(random(aLinks.length));
+	  fill(255);
       rect(linkPosX-20, linkPosY-20, 190, 70, 10);
-      
       textFont('Georgia');      textSize(15);      textLeading(30);      fill(0);
-      text(aLinksText[r], linkPosX, linkPosY, 150, 50);
-      
-      
-      
-      if (mouseX >= linkPosX-30 && mouseY >= linkPosY && mouseX <= linkPosX+190 && mouseY <= linkPosY+50){
+      text(aLinks[r].text, linkPosX, linkPosY, 150, 50);
+      console.log("opened: " + aLinks[r]);
+      console.log("--random num generated --is: "+ r +" --with link name: " + aLinksText[r] + " --and link address: " + aLinksText[r] + "\n\n");
+	if (openLink && aLinks.length){
         window.open(aLinks[r]);
-      }
-      //linkPosX += windowWidth*0.12;
-      linkPosX += 200;
-    }
-    btnShow = false;
-  }
+		linkPosX += 200;
+		openLink = false;
+		aLinks=[];
+		r = null;
+	} return;
 }
 function mouseClicked(){
-  linkPosX= round(windowWidth*(1/7), 2);
-  linkPosY = round(windowHeight*0.8 + 70, 2);
-  for (let i=1; i < 7; i++){
-    if (mouseX > linkPosX-30 && mouseY > linkPosY && mouseX < linkPosX+190 && mouseY < linkPosY+50){
-      coords= [mouseX, mouseY];
-      lines(btnShow=true, coords);
-      console.log("Mouse has been clicked --INSIDE--. at mouseX: "+mouseX+"  and mouseY: " + mouseY);
-      linkPosX += 200;
-    } else {
-      console.log( "mouse has been clicked --OUTSIDE--. at mouseX: "+mouseX+"  and mouseY: " + mouseY);
-    }
-  }
-
-  return;
+	if (mouseX > linkPosX-30 && mouseY > linkPosY && mouseX < linkPosX+190 && mouseY < linkPosY+50){
+		lines(coords, openLink=true);
+		r = round(random(aLinks.length));
+		console.log("shuffled R.")
+		console.log("Mouse has been clicked --INSIDE--. at mouseX: "+mouseX+"  and mouseY: " + mouseY);
+	}
+	return;
 }
 // Get scroller Working again.
 function scroller(wContainer) {
-  // if (typed) typed = "";   // instead of this you should make scroller 
+  // if (typed) typed = "";   // instead of this you should make scroller
   console.log("scroller is running, and secondcopy is: ", secondCopy);
 
   sizeOfText = textWidth(displayT[0]);
@@ -260,7 +216,7 @@ function scroller(wContainer) {
         displayT.splice(0, 1);
         sizeOfText = displayT[0].length;
         displayT[0] += "    ---size of text : " + sizeOfText + " ---size of counter :  " + sizeOfCounter + " ---in element number of counter :  " + each + "and this is page: " + round(sizeOfText/sizeOfCounter, 2);
-        
+
         lines(htc,typed,displayT[0]);
 
         xinit = 0;
@@ -293,20 +249,20 @@ function scroller(wContainer) {
     return;
   }
 }
-
+// Sets up the Canva and control it's resize measures if that's a word.
 function openCanvas() {
   requestPointerLock();
   let cnv = createCanvas(windowWidth, windowHeight);// take the divide by 2 out for full screen.
   cnv.position(0, 0, 'fixed');
   pixelDensity(1); // for high-denstity displays
   background(200);
-  // textOutput(LABEL);
   return;
 }
+
 // Invoke the event listener for keydowns.
 document.addEventListener('keydown', function (event) {
   // openCanvas();
-  if (event.ctrlKey && event.key === 'y') {
+  if (event.ctrlKey && event.key === 'j') {
     // if (displayT.length > 0){
     alert("Paused Reading");
     // step = 0;
@@ -327,7 +283,6 @@ document.addEventListener('keydown', function (event) {
     typed = "";
     console.log("Started reading");
     loop();
-
   }
 })
 // Invoke the event listener for keydowns.
@@ -370,7 +325,7 @@ function keyPressed() {
         console.log("you pressed one of the TOP number keys: " + key);
         lines(htc=highlightTextCount, htc=key);
         return;
-    } 
+    }
   }
   if (keyCode === 32 && typed.length) {  // check if typed key is the SPACE BAR
     console.log("pressed space.");
@@ -392,7 +347,7 @@ function keyPressed() {
       step -= 30;
       //xinit = 0;
       lines();
-      return;  
+      return;
     }
     return;
   } else if (keyCode === DOWN_ARROW) {
@@ -406,7 +361,7 @@ function keyPressed() {
     } else {
       step += 30;
       lines();
-      return;  
+      return;
     }
     return;
   } else if (keyCode === LEFT_ARROW) {
@@ -417,9 +372,9 @@ function keyPressed() {
       return;
     } else {
       sidespeed -= 1;
-      return;  
+      return;
     }
-   return; 
+   return;
   } else if (keyCode === RIGHT_ARROW) {
     console.log("Right key is pressed.");
     console.log("Pressed the right arrow key and step is: ", step);
@@ -428,7 +383,7 @@ function keyPressed() {
       return;
     } else {
       sidespeed += 1;
-      return;  
+      return;
     }
     return;
   }
@@ -438,25 +393,25 @@ function updateScroll(){
     element.scrollTop = element.scrollHeight;
 }
 // Draw Looper
-function draw() {
+function draw(dislayT) {
   circleY =  130;
   fill(255);
   circle(90, circleY, 110);
-  fill(255);
+  //fill(255);
   circle(90, circleY+130, 110);
-  fill(255);
+  //fill(255);
   circle(90, circleY+260, 110);
-  fill(255);
+  //fill(255);
   circle(90, circleY+390, 110);
-  fill(255);
+  //fill(255);
   circle(90, circleY+520, 110);
   // -------------------------------------------------- \\
   fill(0);
   textSize(13);
-  text("SOTe: " + round(sizeOfText,1) + "\nSOCter: " + round(sizeOfCounter,1), 40, circleY-10, 100, 50);
+  text("Nerd_STATS\nSOTe: " + round(sizeOfText,1) + "\nSOCter: " + round(sizeOfCounter,1), 35, circleY-10, 100, 50);
   text("xinit: " + xinit + "\n xfin: " + xfin, 40, circleY+110, 100, 50);
   text("line: " + (step/30+1) + "\nquadrant/ln: " + round(xinit/fiveC) + "\nsidespeed: " + sidespeed, 40, circleY+240, 100, 50);
-  text("page: " + round(sizeOfText/sizeOfCounter) + "\nHcount: " + highlightTextCount + "\nTyped: " + typed, 40, circleY+370, 100, 50);
+  text("pages:" + round(sizeOfText/sizeOfCounter) + "\nHcount: " + highlightTextCount + "\nTyped: " + typed, 40, circleY+370, 100, 50);
   text("pages left: " + displayT.length + "\nof : " + k + "\n PAGE: " + k-displayT.length, 40, circleY+500, 100, 50);
   //updateScroll();
   if (step >= yfin) {
